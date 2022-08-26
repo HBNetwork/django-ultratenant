187d81c04152c046a33bf66b517544cce037baf3
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2022-08-25 21:17:51 -0300

    Add TenantRouter

---
+++ tests/test_database_router.py
@@ -0,0 +1,37 @@
+import pytest
+
+from ultratenant.database import TenantRouter
+from ultratenant.threadlocal import TENANTLOCAL
+
+
+@pytest.fixture(autouse=True)
+def reset_thread():
+    TENANTLOCAL.reset()
+
+
+def test_db_for_read():
+    TENANTLOCAL.tenant = "TENANT"
+
+    router = TenantRouter()
+
+    assert router.db_for_read() == "TENANT"
+
+
+def test_db_for_write():
+    TENANTLOCAL.tenant = "TENANT2"
+
+    router = TenantRouter()
+
+    assert router.db_for_write() == "TENANT2"
+
+
+def test_allow_relation():
+    router = TenantRouter()
+
+    assert router.allow_relation() is True
+
+
+def test_allow_migrate():
+    router = TenantRouter()
+
+    assert router.allow_migrate() is True
